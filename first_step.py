98daa5b9f439a395e841f95dcda1af6eaf858904
Dmitriy Sidorkov <Dima.Sidorkov@gmail.com>
2024-01-18 17:55:56 +0300

    1). Осуществлена оптимизация производительности кода: - добавлена генераторная функция get_url_page_product; - исключен накопительный список url адресов каждого товара.
    2). Fix Bug

--- first_step.py
+++ first_step.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
+user_agent = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 YaBrowser/23.11.0.0 Safari/537.36'
+}  # Необходимо чтобы не получить блокировку сайта на подозрительный запрос
 
 def response(url_site_response, user_agent_response, filter_value):
     """Функция сбора требуемого HTML-кода страницы для последующего анализа элементов"""
@@ -12,25 +15,19 @@ def response(url_site_response, user_agent_response, filter_value):
     return data  # Возвращаем список отфильтрованного HTML-кода
 
 
-user_agent = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 YaBrowser/23.11.0.0 Safari/537.36'
-}  # Необходимо чтобы не получить блокировку сайта на подозрительный запрос
-list_card_url = []
-def card_url():
+def get_url_page_product():
+    for page_number in range(1, 8):  # Запускаем цикл в соответствии с кол-вом страниц с которых требуется получить HTML-код
+        url_site = f'https://scrapingclub.com/exercise/list_basic/?page={page_number}'
+        for data_page in response(url_site, user_agent, filter_value="w-full rounded border"):  # Запускаем цикл по списку отфильтрованного HTML-кода с товарами
+            url_page_product = 'https://scrapingclub.com' + data_page.find('a').get('href')  # Получаем url адрес на страницу конкретного товара
+            yield url_page_product
 
 
-for page_number in range(1, 8):  # Запускаем цикл в соответствии с кол-вом страниц с которых требуется получить HTML-код
+for url_page_product in get_url_page_product():
     sleep(3)  # Необходимо чтобы не получить блокировку сайта на подозрительный запрос
-    url_site = f'https://scrapingclub.com/exercise/list_basic/?page={page_number}'
-    for card in response(url_site, user_agent, filter_value="w-full rounded border"):
-        list_card_url.append('https://scrapingclub.com' + card.find('a').get('href'))
-    # print(*list_card_url, sep='\n')
-for card_url in list_card_url:
-    data_card = response(card_url, user_agent, filter_value='my-8 w-full rounded border')
-    for elem in data_card:
-        name_product = elem.find('h3').text.strip()
-        price_product = elem.find('h4').text.strip()
-        description_product = elem.find('p').text.strip()
-        url_img_product = 'https://scrapingclub.com' + elem.find('img').get('src')
+    for data_product in response(url_page_product, user_agent, filter_value='my-8 w-full rounded border'):  # Запускаем цикл по списку отфильтрованного HTML-кода со страницы конкретного товара
+        name_product = data_product.find('h3').text.strip()
+        price_product = data_product.find('h4').text.strip()
+        description_product = data_product.find('p').text.strip()
+        url_img_product = 'https://scrapingclub.com' + data_product.find('img').get('src')
         print(name_product, price_product, description_product, url_img_product, sep='\n', end='\n\n')
-    # print(name, price, url_img, sep='\n', end='\n\n')
