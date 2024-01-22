d4027d586ef7acffe1f741a09c6e33514b46d655
Dmitriy Sidorkov <Dima.Sidorkov@gmail.com>
2024-01-22 17:08:02 +0300

    Commit 4 Файл parsing_website.py: 1). Осуществлено переименование файла first_step.py на parsing_website.py; 2). Добавлена функция download_image_product для загрузки изображений товаров; 3). Расширены и скорректированы комментарии по коду.
    Файл Excel_writer.py
    1). Осуществлена загрузка изображений каждого товара в Excel файл;
    2). Осуществлено автоматическое/ручное форматирование строк и ячеек;
    3). Расширены и скорректированы комментарии по коду.

--- Excel_writer.py
+++ Excel_writer.py
@@ -1,22 +1,35 @@
 import xlsxwriter
-from first_step import get_info_product
+from parsing_website import get_info_product
 
 
 def writer(data_product, row=0, column=0):
+    """Функция записи собранной информации по товару в файл Excel"""
+    # Создаем/открываем файл Excel
     book = xlsxwriter.Workbook('Перечень товара.xlsx')
+    # Создаем рабочий лист в Excel файле
     page = book.add_worksheet('Товар')
 
-    page.set_column('A:A', 20)
-    page.set_column('B:B', 20)
-    page.set_column('C:C', 50)
-    page.set_column('D:D', 50)
-
+    # Запускаем цикл по полученному кортежу данных в рамках собранной информации
     for item in data_product():
         for index in range(4):
+            # Осуществляем построчную запись текстовых данных по каждому товару
             page.write(row, column + index, item[index])
+        # Осуществляем построчную запись изображения для каждого товара
+        page.insert_image(row, 4, 'image_product\\' + item[0] + '_' + item[3].split('/')[-1],
+                          {'x_offset': 5, 'y_offset': 5, 'x_scale': 0.1, 'y_scale': 0.09})
+        # Осуществляем ручную настройку высоты ячеек
+        page.set_row(row, 90)
         row += 1
 
+    # Осуществляем автоматическую настройку ширины ячеек
+    page.autofit()
+    # Осуществляем ручную настройку ширины ячейки и автоматического переноса текста
+    page.set_column('C:C', 40, book.add_format({'text_wrap': True}))
+    # Осуществляем ручную настройку ширины ячейки
+    page.set_column('E:E', 15)
     book.close()
 
 
+print('Осуществляется загрузка данных с сайта, дождитесь окончания выполнения программы...')
 writer(get_info_product)
+print('Данные успешно загружены в файл - Перечень товара.xlsx')
